49141d14004dceb67c8b444d527047b147f8bc37
chip <chiposaur@gmail.com>
2020-04-23 16:53:24 -0500

    done.

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -11,9 +11,10 @@ from cpu import *
 # BUT we want to debug, so we have to hard set it.
 # filename = "f:\\Dev\\js\\LAMBDA\\CS\\Unit 2\\Week 3\\Computer-Architecture\\ls8\\examples\\print8.ls8"
 # filename = "f:\\Dev\\js\\LAMBDA\\CS\\Unit 2\\Week 3\\Computer-Architecture\\ls8\\examples\\mult.ls8"
-filename = "f:\\Dev\\js\\LAMBDA\\CS\\Unit 2\\Week 3\\Computer-Architecture\\ls8\\examples\\stack.ls8"
+# filename = "f:\\Dev\\js\\LAMBDA\\CS\\Unit 2\\Week 3\\Computer-Architecture\\ls8\\examples\\stack.ls8"
+filename = "f:\\Dev\\js\\LAMBDA\\CS\\Unit 2\\Week 3\\Computer-Architecture\\ls8\\examples\\call.ls8"
 
+print("\n\n")
 cpu = CPU()
-
 cpu.load(filename)
 cpu.run() 
