b8b43a934867e3f656c9936d8e32e8d814a14d9d
chip <chiposaur@gmail.com>
2020-04-24 12:00:54 -0500

    Done.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -25,10 +25,10 @@ class CPU:
 		self.CALL =	0b01010000		# 80
 		self.HLT =	0b00000001		# 1
 		self.LDI =	0b10000010		# 130
-		self.POP =	0b01000110
-		self.PRN =	0b01000111
-		self.PUSH =	0b01000101
-		self.RET =	0b00010001
+		self.POP =	0b01000110		# 70
+		self.PRN =	0b01000111		# 71
+		self.PUSH =	0b01000101		# 69
+		self.RET =	0b00010001		# 17
 
 		# ALU
 		self.ADD =	0b10100000 		# 160
@@ -68,11 +68,11 @@ class CPU:
 			self.register[reg_a] *= self.register[reg_b]
 		elif op == "CMP":
 			if self.register[reg_a] < self.register[reg_b]:
-				self.flag = 0b00000100
+				self.flags = 0b00000100
 			elif self.register[reg_a] > self.register[reg_b]:
-				self.flag = 0b00000010
+				self.flags = 0b00000010
 			elif self.register[reg_a] == self.register[reg_b]:
-				self.flag = 0b00000001
+				self.flags = 0b00000001
 		else:
 			raise Exception("Unsupported ALU operation")
 
@@ -81,28 +81,35 @@ class CPU:
 	# branchtable defs
 	def comp(self):
 		self.alu("CMP", self.ram[self.pc+1], self.ram[self.pc+2])
+		self.advancepc()
 	
 	def jeq(self):
 		if self.flagequal():
 			regtouse = self.ram[self.pc+1]
 			goto = self.register[regtouse]
 			self.pc = goto
+		else:
+			self.advancepc()
 
 	def jne(self):
 		if not self.flagequal():
 			regtouse = self.ram[self.pc+1]
 			goto = self.register[regtouse]
 			self.pc = goto
+		else:
+			self.advancepc()
 
 	def jmp(self):
 		regtouse = self.ram[self.pc+1]
 		goto = self.register[regtouse]
 		self.pc = goto
+		self.advancepc()
 
 
 
 	def add(self):
 		self.alu("ADD", self.ram[self.pc+1], self.ram[self.pc+2])
+		self.advancepc()
 
 	def call(self):
 		returnto = self.pc + 2				# get addy for returnto
@@ -116,13 +123,15 @@ class CPU:
 	
 	def loadimm(self):
 		self.register[self.ram[self.pc+1]] = self.ram[self.pc+2]
+		self.advancepc()
 
 	def multiply(self):
 		self.alu("MULT", self.ram[self.pc+1], self.ram[self.pc+2])
+		self.advancepc()
 
 	def output(self):
 		print(self.register[self.ram[self.pc+1]])
-		# self.trace("PRN ")
+		self.advancepc()
 
 	def push(self, x = None):
 		self.register[self.sp] -= 1			# DECK crement to get room for a deeper stack
@@ -135,21 +144,19 @@ class CPU:
 
 		stackpos = self.register[self.sp]	# where we at in the stack?
 		self.ram[stackpos] = val			# put the value on the stack
-		# self.trace("Push")
+		self.advancepc()
 
 	def pop(self, ret = False):
 		stackpos = self.register[self.sp]	# get the current stackpointer 
 		val = self.ram[stackpos]			# get the value from ram using stackpointer
-
-		# self.trace("Pop ")
 		self.register[self.sp] += 1			# INN crement for a one shallower stack
 
 		if ret:								# if we're just giving the data back
 			return val						# giv it.
-
 		else:								# Otherwise
 			regpos = self.ram[self.pc+1]	# get the register to use from the next slot of ram
 			self.register[regpos] = val		# put the value into the register
+		self.advancepc()
 
 	def ret(self):
 		returnto = self.pop(True)
@@ -179,8 +186,6 @@ class CPU:
 			
 			# Run Maintenance
 			if dotrace: self.trace(label)
-			if not self.checkpcsetter():
-				self.advancepc()  
 
 
 	def advancepc(self):
@@ -190,7 +195,8 @@ class CPU:
 
 	def checkpcsetter(self):
 		IR = self.ram[self.pc]	
-		return ((IR & 0b00010000) >> 5)
+		setter = ((IR & 0b00010000) >> 4)
+		return setter
 		
 	def flagequal(self):
 		return (self.flags == 1)	
